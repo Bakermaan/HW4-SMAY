010da04ad350e76fac767072d7c449b01fc3613d
NetRanger1 <157410184+NetRanger1@users.noreply.github.com>
2024-02-26 09:17:03 -0600

    Update HW4b.py

--- HW4-SMAY_Chase_Seth/HW4b.py
+++ HW4-SMAY_Chase_Seth/HW4b.py
@@ -1,15 +1,34 @@
 from scipy.optimize import fsolve
 import numpy as np
+"""In this code we are trying to find the roots of two mathematical equations efficiently and determine if
+there are any intersections between their solutions. Both eq1 and eq2 are defined as mathematical
+functions, and the fsolve function from scipy.optimize is then used to find roots of these functions within
+a specified initial range. The find_roots_efficiently function iterates over the initial range, and it uses
+fsolve to find roots and ensure precision with a specified tolerance. The results are then presented
+as sorted lists of unique roots for each equation. Additionally, any intersections between the sets of
+roots are identified."""
 
 # Functions as defined in prompt
 def eq1(x):
-    return x**3 - 3 * np.cos(x)
+    """In this function, we are defining the first equation that the roots are going to be found in.
+    For our parameters, 'x' is the float variable for which the equation is defined, and it is going
+    to return a value that is a result of the equation. In this function, we are importing the numpy
+    cosine function that will calculate the cosine of a given angle 'x'."""
+    return x - 3 * np.cos(x)
 
 def eq2(x):
+    """This function does the same thing as def eq1 does, but it just does it for the diferent formula."""
     return np.cos(2*x) - x**3
 
 # A function to find roots efficiently
 def find_roots_efficiently(func, initial_range, tolerance=1e-5):
+    """In this function, we are finding the roots of a given function within a specified initial range.
+    One of the parameters for this function is 'func', and it is the function where the roots are found.
+    Another parameter is 'initial_range', and it is a numpy array that defines the initial range of
+    values to search for roots. Our last parameter is 'tolerance', and it is a float variable that
+    allows us to input the tolerance for the algorithm. It defaults to 1e-5 unless otherwise changed.
+    This function then outputs a sorted list of unique roots that are found within the specified initial
+    range."""
     roots = set()
     for start in initial_range:
         root, info, ier, _ = fsolve(func, start, full_output=True)
